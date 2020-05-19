2635c4e78b678d453793ea9da129b4b4231ce71e
Dennis Mwangi <dennismwangi.m3@gmail.com>
2020-05-19 19:20:28 +0300

    removed hardwired card properties

--- src/game.js
+++ src/game.js
@@ -1,22 +1,26 @@
 class Game{
-    constructor() {
+    constructor(){
         this.cardsInDeck = [],
         this.cardsOnTable = [],
         this.ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"],
         this.suits = ["diamond", "heart", "club", "spade"],
         this.noneStarters = ["A","2","3","8","J","Q","K"],
         this.questionCards = ["Q", "8"],
+        this.noneAnswers = ["K", "J"],
         this.pickingCards = ["2", "3"],
+        this.requestingCards = ["A"],
+        this.skippingCards = ["J"],
+        this.routingCards = ["K"],
+        this.requestedSuit = null,
         this.players = [],
         this.currentPlayer = {},
         this.issue = 1,
         this.route = 1,
         this.skip =  0,
-        this.requested = {canRequest: false, suit: null},
         this.finish = false
     }
     
-    generateDeck() {
+    generateDeck(){
         if(!(this.cardsInDeck.length < 1) && !(this.cardsOnTable.length < 1)){ return null; }
 
         this.ranks.forEach(rank => {
@@ -93,56 +97,70 @@ class Game{
 
         return this;   
     }
-
-    isASpecialCard(specialRanks){
+    
+    requestSuit(requestedSuit){
         let found = 0;
-        specialRanks.forEach(rank => {
-            if(rank === this.cardsOnTable[this.cardsOnTable.length - 1]["rank"]){
+        this.suits.forEach(suit => {
+            if(suit == requestedSuit){
                 found++;
             }
         });
-        if(found > 0){ 
-            return true;
+        if(found < 1 || !this.currentPlayer.canRequest){return null;}
+        this.requestedSuit = requestedSuit;
+    }
+
+    callCardie(playerId){
+        if((playerId !== this.currentPlayer["playerId"])){return null;}
+        if(this.currentPlayer.cardie){
+            this.currentPlayer.cardie = false;
         }else{
-            return false;
-        }
+            this.currentPlayer.cardie = true;
+        }    
+    }
+
+    isASpecialCard(specialRanks, aspiringCardRank = null){
+        let comparison = this.cardsOnTable[this.cardsOnTable.length - 1]["rank"];
+        if(aspiringCardRank !== null){comparison = aspiringCardRank}
+        let found = 0;
+        specialRanks.forEach(rank => {
+            if(rank === comparison){
+                found++;
+            }
+        });
+        if(found > 0){ return true;}
+        return false;
     }
 
     isAQuestionCardMatch(aspiringCard){
         let lastCard = this.cardsOnTable[this.cardsOnTable.length - 1];
 
-        if(this.isASpecialCard(this.questionCards) && aspiringCard["rank"] != "K" && aspiringCard["rank"] != "J" && (lastCard["rank"] === aspiringCard["rank"] || lastCard["suit"] === aspiringCard["suit"])){ 
+        if(this.isASpecialCard(this.questionCards) && !this.isASpecialCard(this.noneAnswers, aspiringCard["rank"]) && (lastCard["rank"] === aspiringCard["rank"] || lastCard["suit"] === aspiringCard["suit"])){ 
             return true;
-        }else{
-            return false;
         }
-
+        return false;
     }
 
     isAPickingCardMatch(aspiringCard){
         let lastCard = this.cardsOnTable[this.cardsOnTable.length - 1];
 
-        if(this.isASpecialCard(this.pickingCards) && (lastCard["rank"] === aspiringCard["rank"] || aspiringCard["rank"] === "A")){
+        if(this.isASpecialCard(this.pickingCards) && (lastCard["rank"] === aspiringCard["rank"] || this.isASpecialCard(this.requestingCards, aspiringCard["rank"]))){
             return true;
-        }else{
-            return false;
         }
+        return false;
     }
 
     isARequestedCard(){
-        if(this.requested.suit === null){
-            return false;
-        }else{
-            return true;
+        if(this.requestedSuit !== null){
+            return true;  
         }
+        return false;
     }
 
     isARequestedCardMatch(aspiringCard){
-        if(this.isARequestedCard() && (aspiringCard["suit"] === this.requested || aspiringCard["rank"] === "A")){
+        if(this.isARequestedCard() && (aspiringCard["suit"] === this.requestedSuit || aspiringCard["rank"] === "A")){
             return true;
-        }else{
-            return false;
         }
+        return false;
     }
 
     isPlayable(index){
@@ -161,7 +179,7 @@ class Game{
             console.log("current player has not played and its a 2/3");
             return true;
         }else if(!this.currentPlayer.justPlayed && this.isARequestedCardMatch(aspiringCard)){
-            this.requested.suit = null;
+            this.requestedSuit = null;
             console.log("player has not played and there is a requested card");
             return true;
         }else if(!this.currentPlayer.justPlayed && this.issue <= 1 && !this.isARequestedCard() && (lastCard["rank"] == aspiringCard["rank"] || lastCard["suit"] == aspiringCard["suit"])){
@@ -174,7 +192,7 @@ class Game{
     }
 
     addNewPlayer(playerId){
-        this.players.push({playerId:playerId, points:0, cardsInHand: [], justPlayed: false, justPicked: false, cardie:false});
+        this.players.push({playerId:playerId, points:0, cardsInHand: [], justPlayed: false, justPicked: false, canRequest: false, cardie:false});
         return this;
     }
 
@@ -189,31 +207,11 @@ class Game{
     setCurrentPlayer(playerIndex){
         this.currentPlayer.justPlayed = false;
         this.currentPlayer.justPicked = false;
+        this.currentPlayer.canRequest = false;
         this.skip = 0;
-        this.requested.canRequest = false;
         this.currentPlayer = this.players[playerIndex]; 
 
         return this;
-    }
-
-    requestSuit(requestedSuit){
-        let found = 0;
-        this.suits.forEach(suit => {
-            if(suit == requestedSuit){
-                found++;
-            }
-        });
-        if(found < 1 || !this.requested.canRequest){return null;}
-        this.requested.suit = requestedSuit;
-    }
-
-    callCardie(playerId){
-        if((playerId !== this.currentPlayer["playerId"])){return null;}
-        if(this.currentPlayer.cardie){
-            this.currentPlayer.cardie = false;
-        }else{
-            this.currentPlayer.cardie = true;
-        }    
     }
 
     goToNextPlayer(playerId){
@@ -221,24 +219,14 @@ class Game{
 
         this.issueCard();
 
-        let nextPlayer;
-        if(this.route < 0){
-            nextPlayer = this.getPlayerIndex(this.currentPlayer.playerId) - this.skip - 1;
-            console.log("reverse route skip " + this.skip);
-        }else{
-            nextPlayer = this.getPlayerIndex(this.currentPlayer.playerId) + this.skip + 1 ;
-            console.log("forward route skip " + this.skip);
+        let nextPlayer = this.getPlayerIndex(this.currentPlayer.playerId) + (this.skip * this.route) + this.route;
+        console.log(this.route + " route skip " + this.skip);
+
+        while(nextPlayer > this.players.length -1 || nextPlayer < 0){
+            nextPlayer -= (this.players.length * this.route);
+            console.log("finding " + this.route);
         }
 
-        while(nextPlayer > this.players.length -1){
-            nextPlayer -= this.players.length;
-            console.log("finding +");
-        }
-        while(nextPlayer < 0){
-            nextPlayer += this.players.length;
-            console.log("finding -");
-        }
-        
         this.setCurrentPlayer(nextPlayer);
 
         return this;
@@ -249,14 +237,23 @@ class Game{
         this.skip += skip;
         this.route *= route;
         this.currentPlayer.points += points;
-        
-        if(canFinish && this.currentPlayer.cardie && this.currentPlayer.cardsInHand.length > 2){
-            this.currentPlayer.points += 100;
-            this.finish = true;
+
+        if(canFinish && this.currentPlayer.cardie && this.currentPlayer.cardsInHand.length < 2){
+            let found = 0;
+            this.players.forEach(player => {
+                if(player.cardsInHand.length < 1){ found++; }
+            });
+            if(found < 1){ 
+                this.currentPlayer.points += 100;
+                this.finish = true;
+                console.log("finished");
+            }else{
+                console.log("cannot finish since someone is cardless");
+            }
         }
 
         if(this.currentPlayer.cardie && !this.finish){
-            this.currentPlayer.points -= 5;
+            this.currentPlayer.points -= 6;
         }
     }
 
@@ -264,17 +261,17 @@ class Game{
         if((playerId !== this.currentPlayer["playerId"]) || !this.isPlayable(index) || this.currentPlayer.justPicked || this.finish){ return null; }
 
         let aspiringCard = this.currentPlayer.cardsInHand[index];
-        if(aspiringCard["rank"] === "A" && this.issue > 1){
+        if(this.isASpecialCard(this.requestingCards, aspiringCard["rank"]) && this.issue > 1){
             this.setPlay(0, 0, 1, 0);
-        }else if(aspiringCard["rank"] === "A" && this.issue <= 1){
+        }else if(this.isASpecialCard(this.requestingCards, aspiringCard["rank"]) && this.issue <= 1){
             //ask for card
-            this.requested.canRequest = true;
+            this.currentPlayer.canRequest = true;
             this.setPlay(1, 0, 1, 6);
-        }else if(aspiringCard["rank"] === "2" || aspiringCard["rank"] === "3"){
+        }else if(this.isASpecialCard(this.pickingCards, aspiringCard["rank"])){
             this.setPlay(this.issue + parseInt(aspiringCard["rank"]), 0, 1, 5);   
-        }else if(aspiringCard["rank"] === "J"){
+        }else if(this.isASpecialCard(this.skippingCards, aspiringCard["rank"])){
             this.setPlay(1, 1, 1, 3);
-        }else if(aspiringCard["rank"] === "K"){
+        }else if(this.isASpecialCard(this.routingCards, aspiringCard["rank"])){
             this.setPlay(1, 0, -1, 2);
         }else{
             this.setPlay(1, 0, 1, 4, true);
